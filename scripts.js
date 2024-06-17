1ff53466478f1b9b302029a75902aa531a419d09
MatthewMcCusker <mattmccusker4@gmail.com>
2024-06-17 15:27:30 -0400

    Added function to play one round of RPS

--- scripts.js
+++ scripts.js
@@ -19,4 +19,30 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`Tie! Both players chose ${humanChoice}.`)
+        return;
+    }
+    const options = ["rock", "paper", "scissors"];
+    let humanValue = options.indexOf(humanChoice);
+    let computerValue = options.indexOf(computerChoice);
+    if (computerValue == humanValue + 1 % 3) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore);
+console.log(computerScore);
