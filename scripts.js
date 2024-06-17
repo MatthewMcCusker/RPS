a5bffbe20aad264220e6702f13264ae5c7755268
MatthewMcCusker <mattmccusker4@gmail.com>
2024-06-17 15:41:36 -0400

    Implemented a function to play a game of 5 rounds of rock paper scissors

--- scripts.js
+++ scripts.js
@@ -20,29 +20,36 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log(`Tie! Both players chose ${humanChoice}.`)
-        return;
-    }
-    const options = ["rock", "paper", "scissors"];
-    let humanValue = options.indexOf(humanChoice);
-    let computerValue = options.indexOf(computerChoice);
-    if (computerValue == humanValue + 1 % 3) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-        computerScore++;
-    } else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    }
+    
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let playRound = function(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log(`Tie! Both players chose ${humanChoice}.`)
+            return;
+        }
+        const options = ["rock", "paper", "scissors"];
+        let humanValue = options.indexOf(humanChoice);
+        let computerValue = options.indexOf(computerChoice);
+        if (computerValue == humanValue + 1 % 3) {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            computerScore++;
+        } else {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+    }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    for(i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
 
-playRound(humanSelection, computerSelection);
+    if (humanScore == computerScore) console.log("It's a complete tie!");
+    if (humanScore > computerScore) console.log("You win it all!");
+    if (humanScore < computerScore) console.log("You lost to the computer :(");
+}
 
-console.log(humanScore);
-console.log(computerScore);
+playGame();
