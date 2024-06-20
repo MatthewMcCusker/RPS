63c5e7ef22173c0b5348d5a1ed7c2297d22d8225
MatthewMcCusker <mattmccusker4@gmail.com>
2024-06-20 15:04:17 -0400

    Added Buttons for player choices and made each play one round of the game

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
-            return  "rock";
+            return "rock";
         case 1:
             return "paper";
         case 2:
@@ -19,37 +19,29 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    
+let humanScore = 0;
+let computerScore = 0;
+let playRound = function (humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`Tie! Both players chose ${humanChoice}.`)
+        return;
+    }
+    const options = ["rock", "paper", "scissors"];
+    let humanValue = options.indexOf(humanChoice);
+    let computerValue = options.indexOf(computerChoice);
+    if (computerValue == humanValue + 1 % 3) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let playRound = function(humanChoice, computerChoice) {
-        if (humanChoice == computerChoice) {
-            console.log(`Tie! Both players chose ${humanChoice}.`)
-            return;
-        }
-        const options = ["rock", "paper", "scissors"];
-        let humanValue = options.indexOf(humanChoice);
-        let computerValue = options.indexOf(computerChoice);
-        if (computerValue == humanValue + 1 % 3) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-            computerScore++;
-        } else {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-    }
+const buttons = document.querySelectorAll('button');
 
-    for(i = 0; i < 5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore == computerScore) console.log("It's a complete tie!");
-    if (humanScore > computerScore) console.log("You win it all!");
-    if (humanScore < computerScore) console.log("You lost to the computer :(");
-}
-
-playGame();
+buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+        playRound(e.target.textContent.toLowerCase(), getComputerChoice());
+    })
+})
